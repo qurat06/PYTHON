c338098aa72ee250c7bab1aa51591600bc5002dd
qurat06 <quratulayeen06@gmail.com>
2025-04-10 10:59:15 +0530

    Update creat.function.py

--- week4/creat.function.py
+++ week4/creat.function.py
@@ -10,9 +10,9 @@ def creatlist(l,n):
 l1=[]
 l2=[]
 l3=[]
-n1=int(input("Enter limit1"))
-n2=int(input("Enter limit2"))
-n3=int(input("Enter limit3"))
+n1=int(input("Enter limit 1: "))
+n2=int(input("Enter limit 2: "))
+n3=int(input("Enter limit 3: "))
 l1=creatlist(l1,n1)
 print(l1)
 l2=creatlist(l2,n2)
