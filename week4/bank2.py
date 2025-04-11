4ad7b87ef22a7d9b4095b10c3808690cc363e762
qurat06 <quratulayeen06@gmail.com>
2025-04-11 11:04:24 +0530

    Update bank2.py

--- week4/bank2.py
+++ week4/bank2.py
@@ -1,29 +1,64 @@
-def credit(balance,Balance,transaction):
+def credit(balance,transaction,amount):
     
-    Balance=Balance+balance
-    print(Balance)
-    return Balance
+    balance=balance+amount
+    transaction.append(amount)
+    return balance,transaction
     
     
-def debit(balance,Balance,transaction):
+def debit(balance,transaction,amount):
+    if balance<amount:
+        print=int(input("Insufficient amount"))
+    balance-=amount
+    transaction.append(amount)
+    return balance,transaction
     
-    Balance=Balance-balance
-    print(Balance)
-    return Balance
+def check_balance(balance):
+    print("current Balance : ",balance)
     
-def check_Balance(Balance):
-    print("current Balance : ",Balance)
+def show_transaction(transaction):
+    print("transaction",transaction)
     
-def transaction(transaction):
-    print("Transaction History")
-    for i in transaction:
-            print(i)
-        
-        
-Balance=0
+    
+    
+    
+    
+balance=0
 transaction=[]
-Balance=credit(200,Balance,transaction)
-Balance=debit(300,Balance,transaction)
-Balance=credit(2000,Balance,transaction)
-Balance=credit(200,Balance,transaction)
-check_Balance(Balance)
+
+while(True):
+    print("credit")
+    print("debit")
+    print("check balance")
+    print("tansaction are")
+    choice=int(input("enter choice "))
+    if choice==1:
+        amount=int(input("enter the amount to be credited "))
+        balance,transaction=credit(balance,transaction,amount)
+    elif choice==2:
+        amount=int(input("enter the amount to be debited "))
+        balance,transaction=debit(balance,transaction,amount)
+    elif choice==3:
+        check_balance(balance)
+    else:
+        show_transaction(transaction)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
